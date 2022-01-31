b8e8636740ad2d369d72924839ebfde3eced257c
Alice <freshjs.ofte@gmail.com>
2022-01-31 20:32:01 +0200

    Capture and Bubling EventListener

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,79 +1,15 @@
-'use strict';
-/*
-const [closeBtn, openBtn] = document.querySelectorAll('button');
+'use strict'
 
+const btn = document.querySelector('button');
+const div = document.querySelector('#root');
 
-// При наведении мышкой на кнопки поменять местами их содержимое (текст)
-
-const switcher = ({target}) => {
-const closeElement = target === closeBtn ? closeBtn : openBtn;
-const openElement = target === closeBtn ? openBtn : closeBtn;
-
-    const tempText = closeElement.textContent;
-    closeElement.textContent = openElement.textContent;
-    openElement.textContent = tempText;
-
-//closeElement.removeEventListener('mouseenter', switcher);
-//openElement.removeEventListener('mouseenter', switcher);
-
+function clickHandler(event) {
+console.dir(event.currentTarget)
 }
 
 
-
-closeBtn.addEventListener('mouseenter', switcher);
-openBtn.addEventListener('mouseenter', switcher);
-
-
-*/
-/* ------------- */
-
-
-
-// const [loginBtn, signInBtn] = document.querySelectorAll('button');
-
-// function clickHandler({target}) {
-//    console.log(target.dataset.userAttr);
-// }
-
-// loginBtn.addEventListener('click', clickHandler);
-// signInBtn.addEventListener('click', clickHandler);
-
-
-/* ------------ */
-
-
-
-
-// Task: При нажатии на кнопку фоновый цвет div-а должен стать такого цвета, как написано на кнопке
-
-const buttons = document.querySelectorAll('button');
-const divRoot = document.querySelector('#root'); // O(1)
-
-//Variant 1
-for (const btn of buttons){
-    btn.addEventListener('click', ({
-        target: {
-            dataset:{
-                color
-            },
-            parentElement: div
-        }}) => {
-        div.style.backgroundColor = color;
-    })
-
-}
-
-//Variant 2
-const handler = ({
-    target: {
-        dataset:{
-            color
-        },
-        parentElement: div
-    }}) => {
-    div.style.backgroundColor = color;
-}
-
-for (const btn of buttons){
-    btn.addEventListener('click', handler)
-}
+btn.addEventListener('click', clickHandler, true);
+div.addEventListener('click', clickHandler, false);
+document.body.addEventListener('click', clickHandler, true);
+document.addEventListener('click', clickHandler, true);
+window.addEventListener('click', clickHandler, true);
