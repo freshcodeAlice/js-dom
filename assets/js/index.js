6d593ec80131929bdfc649354967a0f7b67755c8
Alice <freshjs.ofte@gmail.com>
2022-02-03 21:03:03 +0200

    Form task

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,8 @@
 
 const form = document.getElementById('root-form');
 
+const state = [];
+
 
 form.addEventListener('submit', handleSubmit);
 
@@ -15,6 +17,20 @@ const {target,
         }
     }
 } = event;
-
+console.dir(target);
 console.log(value); // form -> input[name='email'] -> value
 }
+
+
+
+/* 
+1. Создать форму с инпутом и кнопкой. При каждом сабмите формы, вытаскивать значение инпута, валидировать его 
+
+- пустые значения не подходят
+- Переводим строку в нижний регистр
+
+2. После этого пушим полученнное значение в глобальный массив state.
+
+3. Отрендерить на странице список из всех элементов массива
+
+*/
