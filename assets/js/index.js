63c055ca5b9d1467e696bdee654afc833aa2b257
Alice <freshjs.ofte@gmail.com>
2022-02-03 19:47:22 +0200

    Event Methods

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,24 @@
 'use strict'
+
+const form = document.getElementById('root-form');
+
+
+// form.addEventListener('submit', handleSubmit);
+
+// function handleSubmit(event) {
+//     event.preventDefault(); // предотвращает поведение по-умолчанию
+
+
+
+// console.log(event.target);
+// }
+
+
+document.addEventListener('click', (event) =>{
+    event.stopPropagation(); //предотвращает всплытие события
+console.log('click document');
+})
+
+window.addEventListener('click', () => {
+ console.log('click window')   
+})
