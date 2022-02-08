ada99d8c67a8db905bfd080a40d08fc714725438
Alice <freshjs.ofte@gmail.com>
2022-02-08 21:02:37 +0200

    EventLoop and Timeout

---
+++ assets/js/sandbox.js
@@ -0,0 +1,36 @@
+//console.log('1');
+
+//const timeoutId = setInterval(()=>{console.log('tick')}, 1000);
+
+//console.log('3');
+
+//clearInterval(timeoutId)
+
+/* Функция, которая последовательно выводит в консоль числа от 1 до 20 с интервалом 100мс.
+
+Решить можно двумя способами,
+setTimeout - рекурсия с if'ом
+setInterval - if, clearInterval
+
+
+Как измерить время выполнения функции:
+console.time('1');
+console.timeEnd('1');
+
+*/
+
+function count() {
+    let i = 1;
+
+    const id = setInterval(()=>{
+        console.log(i++);
+        if(i>20){
+            clearInterval(id);
+            console.timeEnd('1');
+        }
+    },100)
+
+}
+
+console.time('1');
+count();
