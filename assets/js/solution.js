de49d90f6320710a419c8336e61dec97e39afdd0
Alice <freshjs.ofte@gmail.com>
2022-02-08 19:30:10 +0200

    First solution

--- assets/js/solution.js
+++ assets/js/solution.js
@@ -1,6 +1,22 @@
 // new URL
 
-//new Map
-//.set('facebook.com', 'img_facebook.jpg');
-//.set('instagram.com', 'img_insta.jpg');
-//.set('twitter.com', 'img_twitter.jpg');
+/* ['https://www.instagram.com/leonardodicaprio/', 
+    'https://twitter.com/leodicaprio',
+    'https://www.facebook.com/LeonardoDiCaprio']*/
+
+const SUPPORTED_SOC_LINK = new Map();
+SUPPORTED_SOC_LINK.set('www.facebook.com', 'img_facebook.jpg');
+SUPPORTED_SOC_LINK.set('www.instagram.com', 'img_insta.jpg');
+SUPPORTED_SOC_LINK.set('twitter.com', 'img_twitter.jpg');
+
+function createSocLinks(contacts) {
+const socArr = contacts.map((contact)=>{
+    const url = new URL(contact);
+    const mapkey = url.hostname;
+    const icon = SUPPORTED_SOC_LINK.get(mapkey);
+    const img = createElement('img', {attributes: {src: icon}})
+    const a = createElement('a', {attributes: {href: contact}}, img);
+
+    return a;
+});
+}
