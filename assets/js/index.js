65bc6a0e1928eb26bcf680307decc151f669e002
Alice <freshjs.ofte@gmail.com>
2022-02-09 18:58:46 +0200

    JSON stringify/parse

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,31 @@
 'use strict'
+'use strict'
+
+
+const user = {
+    firstName: 'Ivan',
+    lastName: 'Ivanov',
+    age: 35,
+    getName() {
+        return this.firstName;
+    },
+    isSelecter: null,
+    isAgree: undefined,
+    [Symbol('test')]: {},
+    phones: ['4234234', '43243243', '4234234'],
+    friend: {
+        firstName: '1',
+        lastName: '2'
+    }
+};
+
+const jsonObj = JSON.stringify(user);
+
+const returned = JSON.parse(jsonObj);
+
+/* Создать объект, у которого в значениях лежат строка, число, undefined, null, [], {} и два метода.
+
+Сериализовать (JSON.stringify)
+Десериализовать (JSON.parse)
+
+*/
