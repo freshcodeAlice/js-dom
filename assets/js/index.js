3c36f86c0c22edef5fe12d4e6382ede8e0a3fad6
Alice <freshjs.ofte@gmail.com>
2022-02-10 19:00:17 +0200

    First and second promise

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,4 +21,22 @@ loadImage('https://wallpaperaccess.com/full/50.jpg')
 })
 .catch((error) => {
         console.error(error)
+})
+.finally(()=>{
+        console.log('loading is done!')
 });
+
+
+let i = 1;
+
+const myprom = new Promise((res, rej) =>{
+        if(i === 1) {
+                res()
+        } else {
+                rej
+        }
+})
+
+myprom.then(() => {
+        console.log('i really is 1!')
+})
