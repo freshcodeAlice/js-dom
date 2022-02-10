ef0a2b5ff865d516fa6f8ac266ca6157f619e84c
Alice <freshjs.ofte@gmail.com>
2022-02-10 18:31:10 +0200

    Promise contructor

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,24 +7,27 @@
 // - rejected - отклоненный
 
 
-
-fetch('./assets/js/obj.json')
-        .then((response)=>{
-                return response.json();
-        })
-        .then((data) => {
-               return data
-        })
-        .catch((error)=>{
-            console.log('SOME ERROR!', error)
-        });
-
-
-
+const mypromise = new Promise(function(resolve, reject){
+       if(2+2 === 5) {
+               resolve('ok');
+       } else {
+               reject('not ok');
+       }
+});
+mypromise.then((ok)=>{
+console.log(ok);
+}, (error)=>{
+console.log(error);
+})
 
 
+setTimeout(callback, ms);
+  
+delay(ms).then(calback);
 
 
-
-
-        
+function delay(ms){
+return new Promise((resolve, reject) => {
+        setTimeout(resolve, ms);
+})
+}
