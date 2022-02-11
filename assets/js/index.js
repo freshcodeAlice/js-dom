b73ec10722ce56087044d4bc4ee03ece5f649480
Alice <freshjs.ofte@gmail.com>
2022-02-11 21:03:41 +0200

    Weather widget (almost done)

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,31 +1,58 @@
 'use strict'
 
-function sum(a,b) {
-    return a+b;
+const API_KEY = 'e9f1a9677077d82287315efbf537d64a';
+const API_BASE = 'https://api.openweathermap.org/data/2.5/weather'; 
+
+const divParentWidget = document.querySelector('.weather-block');
+
+const form = document.getElementById('weather-form');
+
+form.addEventListener('submit', formRequestHandler);
+
+function formRequestHandler(event) {
+    event.preventDefault();
+    const {target: {city: {value: chosenCity}}} = event;
+    getRequestData(chosenCity);
 }
 
-function multyply(a,b){
-    return a*b;
+function getRequestData(city) {
+    const url = `${API_BASE}?q=${city}&appid=${API_KEY}&units=metric`;
+
+    fetch(url)
+    .then((response)=>{
+        return response.json()
+    })
+    .then((data) => {
+        createWidget(data);
+    })
 }
 
-function withLog(func, ...args){
-    const res = func(...args);
-    console.log(res);
-    return res;
+function createWidget(weatherData){
+const {main, name, weather, wind, sys} = weatherData;
+
+const widgetBlock = document.createElement('div');
+widgetBlock.classList.add('widget-block');
+
+widgetBlock.append(createWidgetRow('City name: ', name));
+widgetBlock.append(createWidgetRow('Temperature: ', main.temp));
+widgetBlock.append(createWidgetRow('Feels like: ', main.feels_like));
+widgetBlock.append(createWidgetRow('Weather: ', weather[0].description));
+widgetBlock.append(createWidgetRow('Wind speed: ', wind.speed));
+widgetBlock.append(createWidgetRow('Wind deg: ', wind.deg));
+
+// const sunriseDate = new Date(`${sys.sunrise}`);
+// const sunsetDate = new Date(`${sys.sunset}`);
+// widgetBlock.append(createWidgetRow('Sunrise: ', sunriseDate.getTime()));
+// widgetBlock.append(createWidgetRow('Sunset: ', sunsetDate.getTime()));
+
+divParentWidget.append(widgetBlock);
+
 }
 
-//withLog(sum, 2, 3);
+function createWidgetRow(descr, weatherValue){
+    const rowContainer = document.createElement('p');
+    rowContainer.classList.add('widget-row');
+    rowContainer.append(descr, weatherValue);
 
-function withLog2(func){ // Функция-Декоратор
-    return function(...args){
-        const res = func(...args);
-        console.log(res);
-        return res;
-    }
+    return rowContainer;
 }
-
-const sumWithLog = withLog2(sum);
-sumWithLog(2,7);
-
-const multyWithLog = withLog2(multyply);
-multyWithLog(2,2);
