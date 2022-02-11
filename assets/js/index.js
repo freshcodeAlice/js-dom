52d6e700aa163ab868c2a09b99d60d9634434b5c
Alice <freshjs.ofte@gmail.com>
2022-02-11 18:45:25 +0200

    UserError

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,31 +1,31 @@
 'use strict'
-const prom = new Promise(function(resolve, reject){
-    resolve(); // status - fufilled, промис разрешился
+// const prom = new Promise(function(resolve, reject){
+//     resolve(); // status - fufilled, промис разрешился
 
-    // ИЛИ!
+//     // ИЛИ!
 
-    reject(); // status - rejected, промис отклонен
-})
+//     reject(); // status - rejected, промис отклонен
+// })
 
-prom.then(()=>{
-    // resolve!
-}, ()=>{
-    // reject!
-})
+// prom.then(()=>{
+//     // resolve!
+// }, ()=>{
+//     // reject!
+// })
 
-fetch('./assets/js/obj.json')
-.then((response)=>{
-   return response.json();
-})
-.then((data)=>{
-    document.body.append(JSON.stringify(data));
-})
-.catch((error) => {
-    document.body.append('Error happening!');
-})
-.finally(()=>{
-    document.getElementById('load').remove();
-});
+// fetch('./assets/js/obj.json')
+// .then((response)=>{
+//    return response.json();
+// })
+// .then((data)=>{
+//     document.body.append(JSON.stringify(data));
+// })
+// .catch((error) => {
+//     document.body.append('Error happening!');
+// })
+// .finally(()=>{
+//     document.getElementById('load').remove();
+// });
 
 
 
@@ -34,3 +34,12 @@ fetch('./assets/js/obj.json')
 //promise.then(f1, f2)
 
 //promise.then(f1).catch(f2) - is more cool!)
+
+
+class UserError extends Error{
+    constructor(message){
+        super(message);
+        this.userType = 'user';
+        Error.captureStackTrace(this, UserError);
+    }
+}
